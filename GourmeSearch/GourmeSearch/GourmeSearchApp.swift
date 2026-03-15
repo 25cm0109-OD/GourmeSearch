5af5dc2b0f0441bf587f410cbb2e8e6e1a340976
25cm0109oda <25cm0109@jec.ac.jp>
2026-03-15 23:12:54 +0900

    プロジェクトの追加

---
+++ GourmeSearch/GourmeSearch/GourmeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GourmeSearchApp.swift
+//  GourmeSearch
+//
+//  Created by cmStudent on 2026/03/15.
+//
+
+import SwiftUI
+
+@main
+struct GourmeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
